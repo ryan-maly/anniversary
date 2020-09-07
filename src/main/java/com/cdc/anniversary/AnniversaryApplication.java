0b9a97ecc124a17dfdbcf4ece6984db9af8de8e5
ryan-maly <philipskilitor@gmail.com>
2020-09-07 16:59:40 +0800

    项目创建

---
+++ src/main/java/com/cdc/anniversary/AnniversaryApplication.java
@@ -0,0 +1,13 @@
+package com.cdc.anniversary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnniversaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnniversaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdc/anniversary/AnniversaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdc.anniversary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnniversaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
