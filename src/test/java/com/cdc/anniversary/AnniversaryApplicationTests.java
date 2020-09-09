0772df1f7f7c2a22e465aeb2a52cc1c7a7f308ad
ryan-maly <philipskilitor@gmail.com>
2020-09-09 10:09:49 +0800

    add: test

--- src/test/java/com/cdc/anniversary/AnniversaryApplicationTests.java
+++ src/test/java/com/cdc/anniversary/AnniversaryApplicationTests.java
@@ -1,10 +1,12 @@
 package com.cdc.anniversary;
 
+import com.alibaba.fastjson.JSON;
 import com.cdc.anniversary.dto.ShareDTO;
 import com.cdc.anniversary.service.ShareService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,11 +16,8 @@ class AnniversaryApplicationTests {
     ShareService shareService;
 
     @Test
-    void getAllShare() {
-        List<ShareDTO> shareList = shareService.getAllShare(1);
-        for (int i = 0; i < shareList.size(); i++){
-            System.out.println(shareList.get(i));
-        }
+    void getShareByTag(){
+
     }
 
 }
